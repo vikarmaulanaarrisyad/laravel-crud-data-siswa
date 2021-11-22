fa6e3b796d6b1cf7160d07efbc50dff29110f3c2
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gmail.com>
2021-11-22 12:33:00 +0700

    Master Blade Laravel

--- resources/views/siswa/edit.blade.php
+++ resources/views/siswa/edit.blade.php
@@ -1,84 +1,56 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.master')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@section('content')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Edit Siswa</title>
-</head>
-
-<body>
-    <div class="container">
-        <h1>Edit Data Siswa</h1>
-        @if (session('sukses'))
-        <div class="alert alert-success" role="alert">
-            {{ session('sukses') }}
-        </div>
-        @endif
-        <div class="row">
-            <div class="col-lg-12">
-                <form action="/siswa/{{$siswa->id}}/update" method="POST">
-                    {{-- Menyertakan TOken --}}
-                    @csrf
-                    <div class="form-group">
-                        <label for="nama_depan">Nama Depan</label>
-                        <input type="text" class="form-control" id="nama_depan" placeholder="Nama Depan"
-                            name="nama_depan" value="{{ $siswa->nama_depan }}">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="nama_belakang">Nama Belakang</label>
-                        <input type="text" class="form-control" id="nama_belakang" placeholder="Nama Belakang"
-                            name="nama_belakang" name="nama_belakang" value="{{ $siswa->nama_belakang }}">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="exampleFormControlSelect1">Pilih Jenis Kelamin</label>
-                        <select class="form-control" id="exampleFormControlSelect1" name="jenis_kelamin">
-                            <option value="">-- Pilih Jenis Kelamin --</option>
-                            <option value="L" @if($siswa->jenis_kelamin == 'L') selected @endif>Laki-laki</option>
-                            <option value="P" @if($siswa->jenis_kelamin == 'P') selected @endif >Perempuan</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="agama">Agama</label>
-                        <input type="text" class="form-control" id="agama" placeholder="Agama" name="agama" value="{{ $siswa->agama }}">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Alamat</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
-                            name="alamat">{{ $siswa->alamat }}</textarea>
-                    </div>
-
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-warning">Update</button>
-                </form>
+<h1>Edit Data Siswa</h1>
+@if (session('sukses'))
+<div class="alert alert-success" role="alert">
+    {{ session('sukses') }}
+</div>
+@endif
+<div class="row">
+    <div class="col-lg-12">
+        <form action="/siswa/{{$siswa->id}}/update" method="POST">
+            {{-- Menyertakan TOken --}}
+            @csrf
+            <div class="form-group">
+                <label for="nama_depan">Nama Depan</label>
+                <input type="text" class="form-control" id="nama_depan" placeholder="Nama Depan" name="nama_depan"
+                    value="{{ $siswa->nama_depan }}">
             </div>
-        </div>
+
+            <div class="form-group">
+                <label for="nama_belakang">Nama Belakang</label>
+                <input type="text" class="form-control" id="nama_belakang" placeholder="Nama Belakang"
+                    name="nama_belakang" name="nama_belakang" value="{{ $siswa->nama_belakang }}">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Pilih Jenis Kelamin</label>
+                <select class="form-control" id="exampleFormControlSelect1" name="jenis_kelamin">
+                    <option value="">-- Pilih Jenis Kelamin --</option>
+                    <option value="L" @if($siswa->jenis_kelamin == 'L') selected @endif>Laki-laki</option>
+                    <option value="P" @if($siswa->jenis_kelamin == 'P') selected @endif >Perempuan</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="agama">Agama</label>
+                <input type="text" class="form-control" id="agama" placeholder="Agama" name="agama"
+                    value="{{ $siswa->agama }}">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Alamat</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                    name="alamat">{{ $siswa->alamat }}</textarea>
+            </div>
+
+            <button type="close" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-warning">Update</button>
+        </form>
     </div>
+</div>
+{{-- </div> --}}
 
-
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@endsection
--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,133 +1,104 @@
-<!doctype html>
-<html lang="en">
+@extends('layouts.master')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@section('content')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+@if (session('sukses'))
+<div class="alert alert-success" role="alert">
+    {{ session('sukses') }}
+</div>
+@endif
+<div class="row">
+    <div class="col-6">
+        <h2>Data Siswa</h2>
+    </div>
+    <div class="col-6">
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary float-right btn-sm" data-toggle="modal"
+            data-target="#exampleModal">
+            Tambah Data Siswa
+        </button>
+    </div>
+</div>
 
-    <title>Data Siswa</title>
-</head>
+<table class="table table-hover">
+    <tr>
+        <th>NAMA DEPAN</th>
+        <th>NAMA BELAKANG</th>
+        <th>JENIS KELAMIN</th>
+        <th>AGAMA</th>
+        <th>ALAMAT</th>
+        <th>AKSI</th>
+    </tr>
+    @foreach ($data_siswa as $siswa )
+    <tr>
+        <td>{{ $siswa->nama_depan }}</td>
+        <td>{{ $siswa->nama_belakang }}</td>
+        <td>{{ $siswa->jenis_kelamin }}</td>
+        <td>{{ $siswa->agama }}</td>
+        <td>{{ $siswa->alamat }}</td>
+        <td>
+            <a href="/siswa/{{ $siswa->id }}/edit" class="btn btn-warning btn-sm">Edit</a>
+        </td>
+    </tr>
+    @endforeach
+</table>
 
-<body>
-    <div class="container">
-        @if (session('sukses'))
-        <div class="alert alert-success" role="alert">
-            {{ session('sukses') }}
-        </div>
-        @endif
-        <div class="row">
-            <div class="col-6">
-                <h2>Data Siswa</h2>
-            </div>
-            <div class="col-6">
-                <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary float-right btn-sm" data-toggle="modal"
-                    data-target="#exampleModal">
-                    Tambah Data Siswa
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-        </div>
+            <div class="modal-body">
+                <form action="/siswa/create" method="POST">
+                    {{-- Menyertakan TOken --}}
+                    @csrf
+                    <div class="form-group">
+                        <label for="nama_depan">Nama Depan</label>
+                        <input type="text" class="form-control" id="nama_depan" placeholder="Nama Depan"
+                            name="nama_depan">
+                    </div>
 
-        <table class="table table-hover">
-            <tr>
-                <th>NAMA DEPAN</th>
-                <th>NAMA BELAKANG</th>
-                <th>JENIS KELAMIN</th>
-                <th>AGAMA</th>
-                <th>ALAMAT</th>
-                <th>AKSI</th>
-            </tr>
-            @foreach ($data_siswa as $siswa )
-            <tr>
-                <td>{{ $siswa->nama_depan }}</td>
-                <td>{{ $siswa->nama_belakang }}</td>
-                <td>{{ $siswa->jenis_kelamin }}</td>
-                <td>{{ $siswa->agama }}</td>
-                <td>{{ $siswa->alamat }}</td>
-                <td>
-                    <a href="/siswa/{{ $siswa->id }}/edit" class="btn btn-warning btn-sm">Edit</a>
-                </td>
-            </tr>
-            @endforeach
-        </table>
-    </div>
+                    <div class="form-group">
+                        <label for="nama_belakang">Nama Belakang</label>
+                        <input type="text" class="form-control" id="nama_belakang" placeholder="Nama Belakang"
+                            name="nama_belakang">
+                    </div>
 
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form action="/siswa/create" method="POST">
-                        {{-- Menyertakan TOken --}}
-                        @csrf
-                        <div class="form-group">
-                            <label for="nama_depan">Nama Depan</label>
-                            <input type="text" class="form-control" id="nama_depan" placeholder="Nama Depan"
-                                name="nama_depan">
-                        </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlSelect1">Pilih Jenis Kelamin</label>
+                        <select class="form-control" id="exampleFormControlSelect1" name="jenis_kelamin">
+                            <option value="">-- Pilih Jenis Kelamin --</option>
+                            <option value="L">Laki-laki</option>
+                            <option value="P">Perempuan</option>
+                        </select>
+                    </div>
 
-                        <div class="form-group">
-                            <label for="nama_belakang">Nama Belakang</label>
-                            <input type="text" class="form-control" id="nama_belakang" placeholder="Nama Belakang"
-                                name="nama_belakang">
-                        </div>
+                    <div class="form-group">
+                        <label for="agama">Agama</label>
+                        <input type="text" class="form-control" id="agama" placeholder="Agama" name="agama">
+                    </div>
 
-                        <div class="form-group">
-                            <label for="exampleFormControlSelect1">Pilih Jenis Kelamin</label>
-                            <select class="form-control" id="exampleFormControlSelect1" name="jenis_kelamin">
-                                <option value="">-- Pilih Jenis Kelamin --</option>
-                                <option value="L">Laki-laki</option>
-                                <option value="P">Perempuan</option>
-                            </select>
-                        </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Alamat</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                            name="alamat"></textarea>
+                    </div>
 
-                        <div class="form-group">
-                            <label for="agama">Agama</label>
-                            <input type="text" class="form-control" id="agama" placeholder="Agama" name="agama">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="exampleFormControlTextarea1">Alamat</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
-                                name="alamat"></textarea>
-                        </div>
-
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary">Simpan</button>
-                        </div>
-                    </form>
-                </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-    {{-- Akhir Dari Modal --}}
+</div>
 
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@endsection
